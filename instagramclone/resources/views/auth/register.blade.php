fba5846742f6fa9f32ad818fc40f60a819b1c0c1
esumanraju <esumanraju@gmail.com>
2019-08-09 18:09:12 +0530

    add username in register form

--- instagramclone/resources/views/auth/register.blade.php
+++ instagramclone/resources/views/auth/register.blade.php
@@ -25,6 +25,20 @@
                             </div>
                         </div>
 
+                         <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">User Name</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username">
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
